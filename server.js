8d1aada53e5296d84f5eed9be482b6748c195be7
connorw <connorwbest95@gmail.com>
2018-09-15 22:32:56 -0400

    changed port to be dynamic

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var cheerio = require("cheerio");
 // Require all our models
 var db = require("./models");
 
-var PORT = 3000;
+var PORT = process.env.PORT || 8080;
 
 // Initialize Express
 var app = express();
